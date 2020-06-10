89f67048f7eb9a798b55676c3d4ff8b728a5d8cd
vishal <vishal.pardeshi@neosofttech.com>
2020-06-10 20:23:41 +0530

    broken home page into smaller component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections : [{
+                title : 'Hats',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 1
+            },
+            {
+                title : 'Jacket',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 2
+            },
+            {
+                title : 'Sneakers',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 3
+            },
+            {
+                title : 'TShirt',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                size : 'large',
+                id: 4
+            },
+            {
+                title : 'Shoes',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 5
+            }
+        ]
+    };
+}
+
+render() {
+    return (
+        <div className='directory-menu'>
+             {this.state.sections.map(({title, imageUrl, id , size}) =>  (
+                 <MenuItem key={id} title={title} imageUrl={imageUrl} /> 
+             ))}   
+        </div>
+    )}
+}
+
+export default Directory;
