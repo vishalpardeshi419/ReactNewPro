217559e55146a135861fcce392d48212ff864cc5
vishal <vishal.pardeshi@neosofttech.com>
2020-06-18 13:36:46 +0530

    Added collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
 
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'; 
+
+
+const HatsPage = () => {
+  return (
+   <div>
+     <h1>Hats page</h1>
+   </div>
+  );
+};
+
+const TopicPage = () => {
+  return (
+    <div>
+    <h1>Topic page </h1>
+  </div>
+  );
+};
 
 function App() {
   return (
     <div>
-        <HomePage/> 
+      <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={HomePage} /> 
+        <Route exact path='/shop' component={ShopPage} /> 
+        <Route path='/hats/:hatsId' component={TopicPage} /> 
+      </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
